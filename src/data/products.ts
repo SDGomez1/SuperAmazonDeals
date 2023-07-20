6427d7e1959d1900220b7fbf7acb761f8e2d8ac3
Sergio Gomez <sergio.gomez2000@hotmail.com>
2023-07-20 11:47:56 -0500

    [ADD] xbox product

--- src/data/products.ts
+++ src/data/products.ts
@@ -15,10 +15,20 @@ export const Products:Product[] = [
         name: 'AMD Ryzen 9 5900X',
         description: '12-core, 24-Thread Unlocked Desktop Processor',
         price: 162.66,
-        stars: 4,
+        stars: 5,
         link : 'https://amzn.to/3NTuCBj',
         imgUrl: '/productImg/5900x.jpg',
         imgALt: 'Imagen de la caja del procesador'
     },
+    {
+        id: 2,
+        name: 'Xbox Wireless Controller',
+        description: 'Stellar Shift Special Edition Series X|S',
+        price: 52.99,
+        stars: 4,
+        link : 'https://amzn.to/3rrYBZp',
+        imgUrl: '/productImg/xboxStellarShift.jpg',
+        imgALt: 'Imagen del control de xbox'
+    }
     
 ]
