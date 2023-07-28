96e3770a8a58693534b956890f0b01e59634f72c
Sergio Gomez <sergio.gomez2000@hotmail.com>
2023-07-28 13:21:46 -0500

    [ADD] responsive desing

--- src/containers/HowItWorks.tsx
+++ src/containers/HowItWorks.tsx
@@ -34,9 +34,9 @@ export const HowitWorks = () => {
             <p className={styles.number}>3</p>
           </div>
           <div>
-            <p className={styles.textTitle}>Verifica envíos a tu país</p>
+            <p className={styles.textTitle}>Busca en MercadoLibre</p>
             <p className={styles.text}>
-              Asegúrate de que el producto se pueda enviar a tu país.
+              Comprueba si el producto se vende más caro en MercadoLibre{" "}
             </p>
           </div>
         </div>
@@ -45,9 +45,31 @@ export const HowitWorks = () => {
             <p className={styles.number}>4</p>
           </div>
           <div>
-            <p className={styles.textTitle}>Verifica envíos a tu país</p>
+            <p className={styles.textTitle}>Calcula tus ganancias</p>
             <p className={styles.text}>
-              Asegúrate de que el producto se pueda enviar a tu país.
+              Resta la comisión de MercadoLibre para calcular tus ganancias.
+            </p>
+          </div>
+        </div>
+        <div className={styles.gridItem}>
+          <div>
+            <p className={styles.number}>5</p>
+          </div>
+          <div>
+            <p className={styles.textTitle}>Compra y espera</p>
+            <p className={styles.text}>
+              Adquiere el producto en Amazon y espera la entrega.
+            </p>
+          </div>
+        </div>
+        <div className={styles.gridItem}>
+          <div>
+            <p className={styles.number}>6</p>
+          </div>
+          <div>
+            <p className={styles.textTitle}>Vende y aguarda</p>
+            <p className={styles.text}>
+              Pon el producto en venta en MercadoLibre y espera compradores.
             </p>
           </div>
         </div>
